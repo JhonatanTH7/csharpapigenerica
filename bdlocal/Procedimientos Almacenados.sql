466a3c3b4daccedb29f6250390bc6888342c3848
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2025-05-22 06:52:17 -0500

    Procedimientos actualizados

--- bdlocal/Procedimientos Almacenados.sql
+++ bdlocal/Procedimientos Almacenados.sql
@@ -89,141 +89,89 @@ BEGIN
 END
 GO
 
---Procedimiento almacenado para BORRAR la tabla intermedia represenvisualporindicador
-CREATE PROCEDURE BorrarRepresentacionVisualPorIndicador
-    @fkidindicador INT,
-    @fkrepresentacionvisual INT
+CREATE PROCEDURE insertar_indicador_intermedias
+	@codigo VARCHAR(50),
+	@nombre VARCHAR(100),
+	@objetivo VARCHAR(MAX),
+	@alcance VARCHAR(1000),
+	@formula VARCHAR(1000),
+	@meta VARCHAR(1000),
+    @fkidtipoindicador INT,
+    @fkidunidadmedicion INT,
+    @fkidsentido INT,
+    @fkidfrecuencia INT,
+	@fkidarticulo VARCHAR(20) = NULL,
+	@fkidliteral VARCHAR(20)= NULL,
+	@fkidnumeral VARCHAR(20) = NULL,
+	@fkidparagrafo VARCHAR(20) = NULL,
+    @represenvisualporindicador NVARCHAR(MAX) = NULL,
+    @responsablesporindicador NVARCHAR(MAX) = NULL,
+    @fuentesporindicador NVARCHAR(MAX) = NULL,
+    @variablesporindicador NVARCHAR(MAX) = NULL
 AS
-	BEGIN TRANSACTION
-		BEGIN TRY
-			DELETE FROM represenvisualporindicador
-			WHERE fkidindicador = @fkidindicador AND fkidrepresenvisual = @fkrepresentacionvisual;
-			SELECT 1 AS 'Status','Se borro en tabla: RepresentacionVisualPorIndicador' AS 'Respuesta'
-	COMMIT TRANSACTION
-		END TRY
-		BEGIN CATCH
-			DECLARE @ErrorMessage NVARCHAR(4000);
-			SET @ErrorMessage = ERROR_MESSAGE();
-			SELECT 0 AS 'Status', @ErrorMessage AS 'Respuesta'
-	ROLLBACK TRANSACTION
-	END CATCH
 
-GO 
---Procedimiento almacenado para AGREGAR la tabla intermedia represenvisualporindicador
-CREATE PROCEDURE AgregarRepresentacionVisualPorIndicador
-    @fkidindicador INT,
-    @fkrepresentacionvisual INT
-AS
-	BEGIN TRANSACTION
-		BEGIN TRY
-			INSERT INTO represenvisualporindicador
-			VALUES (@fkidindicador,@fkrepresentacionvisual);
-			SELECT 1 AS 'Status','Ingreso el dato' AS 'Respuesta'
-	COMMIT TRANSACTION
-		END TRY
-		BEGIN CATCH
-			DECLARE @ErrorMessage NVARCHAR(4000);
-			SET @ErrorMessage = ERROR_MESSAGE();
-			SELECT 0 AS 'Status', @ErrorMessage AS 'Respuesta'
-	ROLLBACK TRANSACTION
-	END CATCH
-GO
---Procedimiento almacenado para BORRAR la tabla intermedia ResponsablesPorIndicador
-CREATE PROCEDURE BorrarResponsablesPorIndicador
-    @fkidresponsable VARCHAR(50),
-    @fkidindicador INT
-AS
-	BEGIN TRANSACTION
-		BEGIN TRY
-			DELETE FROM responsablesporindicador
-			WHERE fkidresponsable = @fkidresponsable AND fkidindicador = @fkidindicador;
-			SELECT 1 AS 'Status','Se borro en tabla: ResponsablesPorIndicador' AS 'Respuesta'
-	COMMIT TRANSACTION
-		END TRY
-		BEGIN CATCH
-			DECLARE @ErrorMessage NVARCHAR(4000);
-			SET @ErrorMessage = ERROR_MESSAGE();
-			SELECT 0 AS 'Status', @ErrorMessage AS 'Respuesta'
-	ROLLBACK TRANSACTION
-	END CATCH
-GO
---Procedimiento almacenado para AGREGAR la tabla intermedia ResponsablesPorIndicador
-CREATE PROCEDURE AgregarResponsablesPorIndicador
-    @fkidresponsable VARCHAR(50),
-    @fkidindicador INT
-AS
-	BEGIN TRANSACTION
-		BEGIN TRY
-			INSERT INTO responsablesporindicador
-			VALUES (@fkidresponsable,@fkidindicador,GETDATE());
-			SELECT 1 AS 'Status','Se Ingreso en tabla ResponsablesPorIndicador' AS 'Respuesta'
-	COMMIT TRANSACTION
-		END TRY
-		BEGIN CATCH
-			DECLARE @ErrorMessage NVARCHAR(4000);
-			SET @ErrorMessage = ERROR_MESSAGE();
-			SELECT 0 AS 'Status', @ErrorMessage AS 'Respuesta'
-	ROLLBACK TRANSACTION
-	END CATCH
-GO
---Procedimiento almacenado para BORRAR la tabla intermedia ACTOR
-CREATE PROCEDURE Borraractor
-    @id VARCHAR(50)
-AS
-	BEGIN TRANSACTION
-		BEGIN TRY
-			DELETE FROM actor
-			WHERE id = @id;
-			SELECT 1 AS 'Status','Se borro en tabla: ACTOR' AS 'Respuesta'
-	COMMIT TRANSACTION
-		END TRY
-		BEGIN CATCH
-			DECLARE @ErrorMessage NVARCHAR(4000);
-			SET @ErrorMessage = ERROR_MESSAGE();
-			SELECT 0 AS 'Status', @ErrorMessage AS 'Respuesta'
-	ROLLBACK TRANSACTION
-	END CATCH
-GO
---Procedimiento almacenado para AGREGAR la tabla intermedia ResponsablesPorIndicador
-CREATE PROCEDURE AgregarActor
-    @id VARCHAR(50),
-	@nombre varchar(200),
-	@fkidtipoactor int
-AS
-	BEGIN TRANSACTION
-		BEGIN TRY
-			INSERT INTO responsablesporindicador
-			VALUES (@id,@nombre,@fkidtipoactor);
-			SELECT 1 AS 'Status','Se Ingresaron los datos en la tabla: ACTOR exitosamente' AS 'Respuesta'
-	COMMIT TRANSACTION
-		END TRY
-		BEGIN CATCH
-			DECLARE @ErrorMessage NVARCHAR(4000);
-			SET @ErrorMessage = ERROR_MESSAGE();
-			SELECT 0 AS 'Status', @ErrorMessage AS 'Respuesta'
-	ROLLBACK TRANSACTION
-	END CATCH
-GO
---Procedimiento almacenado para AGREGAR la tabla intermedia ResponsablesPorIndicador
-CREATE PROCEDURE AgregarVariablesporIndicador
-	@id INT,
-	@fkidvariable int,
-	@fkidindicador int,
-	@dato float,
-	@fkemailusuario varchar(100),
-	@fechadato datetime
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY
+		BEGIN TRANSACTION;
+	
+		INSERT INTO indicador (codigo,nombre,objetivo,alcance,formula,meta,fkidtipoindicador,fkidunidadmedicion,fkidsentido,fkidfrecuencia,fkidarticulo,fkidliteral,fkidnumeral,fkidparagrafo)
+		VALUES (@codigo,@nombre,@objetivo,@alcance,@formula,@meta,@fkidtipoindicador,@fkidunidadmedicion,@fkidsentido,@fkidfrecuencia,@fkidarticulo,@fkidliteral,@fkidnumeral,@fkidparagrafo)
+		DECLARE @nuevoNumeroIndicador INT = SCOPE_IDENTITY();
 
-AS
-	BEGIN TRANSACTION
-		BEGIN TRY
-			INSERT INTO VariablesporIndicador
-			VALUES (@fkidvariable,@fkidindicador,@dato,@fkemailusuario,@fechadato);
-			SELECT 1 AS 'Status','Se Ingreso en tabla:  FuentesPorIndicador' AS 'Respuesta'
-	COMMIT TRANSACTION
-		END TRY
-		BEGIN CATCH
-			DECLARE @ErrorMessage NVARCHAR(4000);
-			SET @ErrorMessage = ERROR_MESSAGE();
-			SELECT 0 AS 'Status', @ErrorMessage AS 'Respuesta'
-	ROLLBACK TRANSACTION
-	END CATCH
+		INSERT INTO represenvisualporindicador (fkidindicador, fkidrepresenvisual)
+		SELECT @nuevoNumeroIndicador, fkidrepresenvisual
+		FROM OPENJSON(@represenvisualporindicador)
+		WITH (
+			fkidrepresenvisual INT '$.fkidrepresenvisual'
+		);
+
+		INSERT INTO responsablesporindicador (fkidindicador, fkidresponsable)
+		SELECT @nuevoNumeroIndicador, fkidresponsable
+		FROM OPENJSON(@responsablesporindicador)
+		WITH (
+			fkidresponsable VARCHAR(50) '$.fkidresponsable'
+		);
+
+		INSERT INTO fuentesporindicador (fkidindicador, fkidfuente)
+		SELECT @nuevoNumeroIndicador, fkidfuente
+		FROM OPENJSON(@fuentesporindicador)
+		WITH (
+			fkidfuente INT '$.fkidfuente'
+		);
+
+		INSERT INTO variablesporindicador (fkidindicador, fkidvariable, dato, fkemailusuario, fechadato)
+		SELECT @nuevoNumeroIndicador, fkidvariable, dato,fkemailusuario, GETDATE()
+		FROM OPENJSON(@variablesporindicador)
+		WITH (
+			fkidvariable INT '$.fkidvariable',
+			dato FLOAT '$.dato',
+			fkemailusuario VARCHAR(100) '$.fkemailusuario'
+		);
+
+		COMMIT TRANSACTION;
+
+		SELECT 
+            @nuevoNumeroIndicador AS NumeroIndicador, 
+            'Indicador creado exitosamente' AS Mensaje,
+            GETDATE() AS FechaCreacion;
+
+	END TRY
+	BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        
+        -- En caso de error, devolver información sobre el error
+        SELECT 
+            0 AS NumeroIndicador,
+            @ErrorMessage AS Mensaje,
+            GETDATE() AS FechaCreacion,
+            ERROR_NUMBER() AS ErrorNumero,
+            ERROR_STATE() AS ErrorEstado,
+            ERROR_SEVERITY() AS ErrorSeveridad,
+            ERROR_LINE() AS ErrorLinea;
+            
+        RAISERROR('Error al insertar indicador: %s', 16, 1, @ErrorMessage);
+    END CATCH
+END;
+GO
